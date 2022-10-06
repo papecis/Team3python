0cf87c3751197c87efcfd29daffd5d651d60868d
Gaoussou Cissoko <81573754+papecis@users.noreply.github.com>
2022-10-05 22:56:22 -0400

    Update teamc.py

--- teamc.py
+++ teamc.py
@@ -1,11 +1,12 @@
-import random
 import math
+import random
 import winsound
 
 print("Hello what is your name?")
 person = input()
 print("Okay", person, "welcome to the guessing game", "would you like to play?",
       "\n I will choose a number from a range of numbers you will input. Can you guess the number I choose?\n")
+
 # Taking Inputs
 lower = int(input("Enter Lower bound:- "))
 
@@ -22,28 +23,32 @@ print("\nYou've only ",
 # Initializing the number of guesses.
 count = 0
 # for calculation of minimum number of
-# guesses depends upon range
+# guesses depends upon random
+
 while count < math.log(upper - lower + 1, 2):
+
     count += 1
     # taking guessing number as input
     guess = int(input("Guess a number:- "))
 
     # Condition testing
-    if x == guess:
-        print("Congratulations", person, "you are the wiser and you did it in", count, "try", "\U0001f600")
-        # Once guessed, loop will break
-        break
-    elif x > guess:
+    if guess == x:
+        winsound.PlaySound("YAY.wav", winsound.SND_LOOP)
+        print("Congratulation", person, "you are the wiser and you did it in ", count, " try", "\U0001f600")
+        exit()
+
+    elif guess < x:
         print("You guessed too small!")
-    elif x < guess:
+    elif guess > x:
         print("You guessed too high!")
 
 # If Guessing is more than required guesses,
 # shows this output.
 if count >= math.log(upper - lower + 1, 2):
-    winsound.PlaySound("BOO.wav", winsound.SND_LOOP)
+    winsound.PlaySound("Boo.wav", winsound.SND_LOOP)
     print("\nThe guessing number was %d" % x)
-    print("\tBetter Luck", person, "the next time!", "\U0001F606")
+    print("Sorry", person, "better luck next time!", "\U0001F606")
+
 
 
 
